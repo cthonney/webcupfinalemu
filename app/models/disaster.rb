2d3281a9448a0bde33d18e22283722c209e2706e
Thonney Cedric <c.thonney@gmail.com>
2023-10-28 23:43:08 +0300

    add sea if nil

--- app/models/disaster.rb
+++ app/models/disaster.rb
@@ -1,5 +1,11 @@
 class Disaster < ApplicationRecord
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode
+  after_validation :set_default_address
   has_many :reviews, dependent: :destroy
+
+  def set_default_address
+    # Attribuer "sea" si l'attribut de géolocalisation est nil
+    self.address ||= "Sea" if self.address.nil?
+  end
 end
