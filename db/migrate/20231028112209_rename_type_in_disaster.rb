5f4e151c0855737e7d5c17b84c103289686b72f0
Christian Bongard <christian.bongard90@gmail.com>
2023-10-28 17:08:40 +0300

    rails admin functional

--- db/migrate/20231028112209_rename_type_in_disaster.rb
+++
@@ -1,6 +0,0 @@
-class RenameTypeInDisaster < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :disasters, :type, :disaster_type
-    #Ex:- rename_column("admin_users", "pasword","hashed_pasword")
-  end
-end
