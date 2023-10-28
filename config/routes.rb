019ae5ddefbb5e9e9a0bb27051ed67ce78bed5af
Christian Bongard <christian.bongard90@gmail.com>
2023-10-28 18:59:03 +0300

    updated admin and added resources page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "about", to: "pages#about"
   get "contact", to: "pages#contact"
+  get "resources", to: "pages#resources"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
