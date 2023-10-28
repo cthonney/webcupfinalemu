bfdc2d718e2c0dbcfb2cd52685a611bef349ec96
Christian Bongard <christian.bongard90@gmail.com>
2023-10-28 21:22:50 +0300

    added mapbox and popup markers

--- app/controllers/disasters_controller.rb
+++ app/controllers/disasters_controller.rb
@@ -1,6 +1,13 @@
 class DisastersController < ApplicationController
   def index
     @disasters = Disaster.all
+    @markers = @disasters.geocoded.map do |disaster|
+      {
+        lat: disaster.latitude,
+        lng: disaster.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {disaster: disaster})
+      }
+    end
   end
 
   def show
---
+++ app/javascript/controllers/map_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus";
+import mapboxgl from "mapbox-gl"; // Don't forget this!
+
+export default class extends Controller {
+  static values = {
+    apiKey: String,
+    markers: Array,
+  };
+
+  connect() {
+    mapboxgl.accessToken = this.apiKeyValue;
+
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/mapbox/streets-v10",
+    });
+    this.#addMarkersToMap();
+  }
+
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html); // Add this
+      new mapboxgl.Marker()
+        .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup) // Add this
+        .addTo(this.map);
+    });
+  }
+}
