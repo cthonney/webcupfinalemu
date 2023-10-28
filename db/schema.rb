3e9dc512e8639baeef4ce9d0972607d03c7c1f62
Christian Bongard <christian.bongard90@gmail.com>
2023-10-28 21:02:14 +0300

    added reverse geocoding and address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_28_152349) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_28_175635) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_28_152349) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "fetched_id"
+    t.string "address"
   end
 
   create_table "reviews", force: :cascade do |t|
